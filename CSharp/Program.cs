fd72a9da137925100f0391b731b98d71f77173fe
YaeKitsune <aepnaonp@gmail.com>
2024-11-05 14:15:11 +0500

    add C#

---
+++ CSharp/Program.cs
@@ -0,0 +1,11 @@
+﻿
+string funcCheking(string Text){
+    Console.WriteLine(Text);
+    return Text;
+}
+
+var Chenking = new Dictionary<string,object>(){
+    {"Key1",funcCheking("Текст1")}
+};
+
+Chenking['Key1'];
